cd6e9479049370d7facbb0e10b211b43c124d81c
vyompathak9-prog <vyompathak9@gmail.com>
2026-09-14 17:34:16 +0530

    Add files via upload

---
+++ exp1.c
@@ -0,0 +1,8 @@
+#includ <studio.h>
+int main()
+{
+printf("hey I am good ");
+printf("what about you");
+return0;
+}
+
---
+++ exp2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main ()
+{
+printf("My name is Vyom Pathak .I will do all the tast my teacher ask me to do and stude hard");
+return 0;
+}
