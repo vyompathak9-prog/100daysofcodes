c6f048cf4a44f1cf58bb030bed99ea83c1ae374e
vyompathak9-prog <vyompathak9@gmail.com>
2026-09-17 15:14:32 +0530

    Add files via upload

---
+++ opr.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int  main()
+{
+int  a;
+int  b;
+int  oper;
+printf("Enter Number 1\n");
+scanf("%d",&a);
+printf("Enter Number 2\n");
+scanf("%d",&b);
+printf("addition of number isn");
+oper = a+b;
+printf("%d\n",oper);
+printf("multiplication is\n");
+oper =a*b;
+printf("%d\n",oper);
+printf("substraction\n");
+oper =a-b;
+printf("%d\n",oper);
+printf("division of number");
+oper=a/b;
+printf("%d\n",oper);
+
+return 0;
+
+
+
+
+
+
+}
