b260a84910db45050110c91bb22ad0d94fc177e9
vyompathak9-prog <vyompathak9@gmail.com>
2026-09-17 15:28:13 +0530

    Add files via upload

---
+++ swipe.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    // Swapping using third variable
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("After swapping:\n");
+    printf("First number = %d\n", a);
+    printf("Second number = %d\n", b);
+
+    return 0;
+}
