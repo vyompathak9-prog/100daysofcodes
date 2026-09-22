82691eed52012782a920e916cf95aca5dfeb42d1
vyompathak9-prog <vyompathak9@gmail.com>
2026-09-22 23:43:03 +0530

    Add files via upload

---
+++ day10-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a + b <= c || a + c <= b || b + c <= a)
+        printf("Invalid triangle");
+    else if (a == b && b == c)
+        printf("Equilateral");
+    else if (a == b || b == c || a == c)
+        printf("Isosceles");
+    else
+        printf("Scalene");
+
+    return 0;
+}
